e8604f863b4e17dea664a00747061a7ea7edf5bf
Segovia <35817798+SegoCode@users.noreply.github.com>
2021-01-10 20:19:47 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,17 +1,22 @@
 window.onload = function() {
   var fileInput = document.getElementById("fileInput");
-  var fileDisplayArea = document.getElementById("fileDisplayArea");
+  var table = document.getElementById("dataTable");
   fileInput.addEventListener("change", function(e) {
     var file = fileInput.files[0];
     var reader = new FileReader();
     reader.readAsText(file);
+
+    //On load history
     reader.onload = function(e) {
+      var auxNum = 0;
+      var dataLines = this.result.split("\n");
+
+
+      //Get series and Watched episodes
       var series = [];
       var times = [];
-      var auxNum = 0;
-      var lines = this.result.split("\n");
-      for (var line = 1; line < lines.length - 1; line++) {
-        var name = parser(lines[line]);
+      for (var line = 1; line < dataLines.length - 1; line++) {
+        var name = parserName(dataLines[line]);
         if (!series.includes(name)) {
           series.push(name);
           times[auxNum] = 1;
@@ -20,79 +25,112 @@ window.onload = function() {
           times[series.indexOf(name)] = times[series.indexOf(name)] + 1;
         }
       }
-      console.log(series);
-      console.log(times);
-      createPie(series, times);
+
+      //Get visionate times
+      dataLines.reverse();
+      var data = [];
+      var lastYear;
+      auxNum = 0;
+      lastYear = parserLineToYear(dataLines[1]);
+      for (var line = 1; line < dataLines.length - 1; line++) {
+        var yearNew = parserLineToYear(dataLines[line]);
+
+        if (lastYear != yearNew) {
+          data.push({
+            "year": lastYear + "",
+            "episodes": auxNum
+          });
+          for (var j = 1; j < yearNew - lastYear; j++) {
+            data.push({
+              "year": lastYear + j + "",
+              "episodes": 0
+            });
+          }
+
+          lastYear = yearNew;
+          auxNum = 1;
+        } else {
+          auxNum = auxNum + 1;
+        }
+      }
+      loadChart(data);
+
+      //load table
       for (i = 0; i < series.length; i++) {
         var newRow = table.insertRow(table.length);
         newRow.insertCell(0).innerHTML = series[i];
         newRow.insertCell(1).innerHTML = times[i];
       }
+
     };
   });
 };
 
-function parser(data) {
-  if (data.includes(":")) {
-    data = data.split(":")[0];
+function parserLineToYear(stringDataLine) {
+  stringDataLine = (stringDataLine.split("\",\"")[1]).replace(/"/g, "");
+  return parseInt("20" + stringDataLine.split("/")[2]);
+}
+
+
+
+function parserName(StringLine) {
+  if (StringLine.includes(":")) {
+    StringLine = StringLine.split(":")[0];
   } else {
-    data = data.split(",")[0];
+    StringLine = StringLine.split(",")[0];
   }
-  data = data.replace(/"/g, "");
-  return data;
+  StringLine = StringLine.replace(/"/g, "");
+
+  return StringLine;
 }
 
-// Chart.js
-function createPie(labels, views) {
-  var canvasP = document.getElementById("pieChart");
-  var ctxP = canvasP.getContext("2d");
-  var colors = generateColors(views);
-  var myPieChart = new Chart(ctxP, {
-    type: "doughnut",
-    data: {
-      labels: labels,
-      datasets: [
-        {
-          backgroundColor: colors,
-          data: views
-        }
-      ]
-    },
-    options: {
-      legend: {
-        display: false
-      }
-    }
-  });
-}
 
-function generateColors(views) {
-  var colors = [];
-  for (i = 0; i < views.length; i++) {
-    colors[i] = getRandomColor();
-  }
-  return colors;
-}
+function loadChart(data) {
 
-function getRandomColor() {
-  var letters = "0123456789ABCDEF".split("");
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+  am4core.useTheme(am4themes_animated);
 
-// Collapse panel
-var coll = document.getElementsByClassName("collapsible");
-for (var i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+  var chart = am4core.create("chartdiv", am4charts.XYChart);
+
+  chart.data = data;
+
+  var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+  categoryAxis.renderer.grid.template.location = 0;
+  categoryAxis.renderer.ticks.template.disabled = true;
+  categoryAxis.renderer.line.opacity = 0;
+  categoryAxis.renderer.grid.template.disabled = true;
+  categoryAxis.renderer.minGridDistance = 40;
+  categoryAxis.dataFields.category = "year";
+  categoryAxis.startLocation = 0.4;
+  categoryAxis.endLocation = 0.6;
+
+
+  var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+  valueAxis.tooltip.disabled = true;
+  valueAxis.renderer.line.opacity = 0;
+  valueAxis.renderer.ticks.template.disabled = true;
+  valueAxis.min = 0;
+
+  var lineSeries = chart.series.push(new am4charts.LineSeries());
+  lineSeries.dataFields.categoryX = "year";
+  lineSeries.dataFields.valueY = "episodes";
+  lineSeries.tooltipText = "episodes: {valueY.value}";
+  lineSeries.fillOpacity = 0.5;
+  lineSeries.strokeWidth = 3;
+  lineSeries.propertyFields.stroke = "lineColor";
+  lineSeries.propertyFields.fill = "lineColor";
+
+  var bullet = lineSeries.bullets.push(new am4charts.CircleBullet());
+  bullet.circle.radius = 6;
+  bullet.circle.fill = am4core.color("#fff");
+  bullet.circle.strokeWidth = 3;
+
+  chart.cursor = new am4charts.XYCursor();
+  chart.cursor.behavior = "panX";
+  chart.cursor.lineX.opacity = 0;
+  chart.cursor.lineY.opacity = 0;
+
+  chart.scrollbarX = new am4core.Scrollbar();
+  chart.scrollbarX.parent = chart.bottomAxesContainer;
+
+  document.getElementById("chartdiv").style.display = "";
 }
